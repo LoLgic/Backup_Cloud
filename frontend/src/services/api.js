0f9a252a2dfaba2d26b17bdc8eb72e04542a163f
Mario Petro <sample@example.com>
2026-05-11 00:07:13 -0500

    Fix API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backup-cloud.onrender.com"
+  baseURL: "https://backup-cloud.onrender.com/api"
 });
 
 // Interceptor JWT
