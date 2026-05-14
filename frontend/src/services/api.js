8a2623f7f7cd25eedec2fa36d0e1918dc3602c51
Mario Petro <sample@example.com>
2026-05-14 13:25:05 -0500

    refresh token frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,4 +17,74 @@ api.interceptors.request.use((config) => {
 
 });
 
+
+api.interceptors.response.use(
+
+  (response) => response,
+
+  async (error) => {
+
+    const originalRequest =
+      error.config;
+
+    // Token expirado
+    if (
+
+      error.response?.status === 401
+      &&
+      !originalRequest._retry
+
+    ) {
+
+      originalRequest._retry = true;
+
+      try {
+
+        const refreshToken =
+          localStorage.getItem(
+            "refreshToken"
+          );
+
+        const response =
+          await axios.post(
+
+            "https://backup-cloud.onrender.com/api/token/refresh",
+
+            {
+              refreshToken
+            }
+
+          );
+
+        const newAccessToken =
+          response.data.accessToken;
+
+        localStorage.setItem(
+          "token",
+          newAccessToken
+        );
+
+        originalRequest.headers.Authorization =
+          `Bearer ${newAccessToken}`;
+
+        return api(
+          originalRequest
+        );
+
+      } catch (refreshError) {
+
+        localStorage.clear();
+
+        window.location.href =
+          "/login";
+
+      }
+
+    }
+
+    return Promise.reject(error);
+
+  }
+);
+
 export default api;
